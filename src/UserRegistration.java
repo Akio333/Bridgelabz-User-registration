4d5772211c4b4fdbee6f53cd84d7cbbc813ec34a
Akio333 <smulye10@gmail.com>
2021-01-13 21:59:14 +0530

    ADDED: Validation of email id

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -12,6 +12,8 @@ public class UserRegistration {
         userRegistration.nameValidation(scanner.next());
         System.out.println("Enter First Name: ");
         userRegistration.nameValidation(scanner.next());
+        System.out.println("Enter Email id: ");
+        userRegistration.emaileValidation(scanner.next());
 
     }
 
@@ -20,6 +22,14 @@ public class UserRegistration {
             System.out.println("Valid Name");
         } else {
             System.out.println("Invalid Name");
+        }
+    }
+
+    public void emaileValidation(String name) {
+        if (Pattern.matches("^[A-Za-z0-9_][A-Za-z0-9_.]*[@]{1}[a-z]+[.][a-z]{2,6}$", name)) {
+            System.out.println("Valid Email Id");
+        } else {
+            System.out.println("Invalid Email Id");
         }
     }
 }
